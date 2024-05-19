385d05b55f134164ea87a3889cddf9b525ef27f6
bruno medve <brunomedve@outlook.com>
2024-05-19 19:42:57 -0300

    commit operador ternario

---
+++ OperadorTernario/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        int a, b;
+        a= 5;
+        b= 6;
+
+        String resultado = a==b ? "verdadeiro" : "falso";
+
+        System.out.println(resultado);
+    }
+}
