32900a0376c6d789d8a4aaf1b552fb21fbf33768
bruno medve <brunomedve@outlook.com>
2024-05-23 21:01:21 -0300

    commit condicional

---
+++ Condicional-commposta/src/Condicional.java
@@ -0,0 +1,11 @@
+public class Condicional {
+    public static void main(String[] args) throws Exception {
+
+        int nota = 8;
+
+        if (nota >= 6) {
+            System.out.println("Aprovado");
+        } else
+            System.out.println("Reprovado");
+    }
+}
---
+++ Condicional-commposta/src/CondicionalTernaria.java
@@ -0,0 +1,11 @@
+public class CondicionalTernaria {
+    
+    public static void main(String[] args) {
+        
+        int nota = 7;
+
+        String resultado = nota >=7 ? "Aprovado" : "Repreovado";
+
+        System.out.println(resultado);
+    }
+}
